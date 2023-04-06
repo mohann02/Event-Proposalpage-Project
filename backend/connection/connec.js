d04e178d2e6de776a4846b66a26420e5e4884f3d
Mohan Kumar <mohanneelmani@gmail.com>
2023-04-06 23:45:37 +0530

    Some changes done

--- backend/connection/connec.js
+++ backend/connection/connec.js
@@ -2,7 +2,11 @@ const mongoose=require('mongoose');
 mongoose.set('strictQuery',false)
 
 async function getConnection(){
-    await mongoose.connect('mongodb+srv://mohan:mohan123@cluster0.zsqta3l.mongodb.net/?retryWrites=true&w=majority');
+    await mongoose.connect('mongodb+srv://mohan:mohan123@cluster0.zsqta3l.mongodb.net/?retryWrites=true&w=majority').then(()=>{
+        console.log("connection successful")
+    }).catch((err)=>{
+        console.log("no connection")
+    })
 }
 
 module.exports=getConnection;
